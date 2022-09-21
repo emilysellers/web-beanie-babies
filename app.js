09185ce4b5af3b48a6d47f2281bd45ff1a84f7ca
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-21 16:54:39 -0700

    add code for getting and displaying beanies, and add render-utils.js

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 /* Imports */
 import { getBeanies } from './fetch-utils.js';
+
 /* Get DOM Elements */
+const beaniesList = document.getElementById('beanies-list');
 
 /* State */
+let beanies = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    findBeanies();
+});
 
+async function findBeanies() {
+    const response = await getBeanies();
+
+    error = response.error;
+    beanies = response.data;
+
+    displayNotifications();
+    if (!error) {
+        displayBeanies();
+    }
+}
 /* Display Functions */
+function displayBeanies() {
+    beaniesList.innerHTML = '';
+
+    for (const beanie of beanies) {
+        const beanieEl = renderBeanie();
+        beaniesList.append(beanieEl);
+    }
+}
+
+function displayNotifications() {}
 
 // (don't forget to call any display functions you want to run on page load!)
+findBeanies();
