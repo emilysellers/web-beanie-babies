817b280519fc35641f5fb99a7f704ff3fbc9f1b4
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-21 17:12:32 -0700

    add render-utils with renderBeanie function

--- render-utils.js
+++ render-utils.js
@@ -1,3 +1,18 @@
-function renderBeanie {
-    
+export function renderBeanie(beanie) {
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    const image = document.createElement('img');
+    image.src = ``;
+    image.alt = beanie.name;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = beanie.name;
+
+    const p = document.createElement('p');
+    p.textContent = beanie.astro;
+
+    li.append(image, h2, p);
+
+    return li;
 }
